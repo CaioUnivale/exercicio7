94fe91ade06007806c541264621843a19b0bf618
CaioUnivale <estudos151927@gmail.com>
2023-04-10 12:36:47 -0300

    first commit

---
+++ script.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+dieimes_matriz = np.array([[3, 4, 1], [3, 1, 2]])
+
+soma = 0 
+
+for linha in dieimes_matriz:
+   
+    for elemento in linha:
+        soma += elemento  
+
+print("A soma de todos os elementos da matriz é:", soma)
